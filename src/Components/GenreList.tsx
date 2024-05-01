66614cd2de2c9d525dcd4a941b1b4e7f935823ba
Pere-Isenah <pereisenah@gmail.com>
2024-05-01 16:18:21 +0100

    update

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import GlobalApi from "../Services/GlobalApi"
+
+
+useEffect(()=> {
+  getGenreList();
+},[])
 
 const GenreList = () => {
+  const getGenreList= () =>{
+    GlobalApi.getGenreList.then((resp)=>{
+      console.log(resp);
+    })
+  }
   return (
     <div>
     genre list
--- src/Services/GlobalApi.tsx
+++ src/Services/GlobalApi.tsx
@@ -1,4 +1,10 @@
 import axios from "axios"
 const key = import.meta.env.VITE_API_KEY
 
-const creatAxios = axios.create({baseURL:"https://api.rawg.io/api"})
+const axiosCreate = axios.create({baseURL:"https://api.rawg.io/api"})
+
+const getGenreList = axiosCreate.get("/genres?key=" +key)
+
+export default {
+  getGenreList
+}
