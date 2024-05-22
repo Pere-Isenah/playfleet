e44fee336f95a089dab887f70e656f4905f08995
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-05-22 22:43:29 +0100

    feat: create hook to fetch games list by genre

--- src/Services/GlobalApi.tsx
+++ src/Services/GlobalApi.tsx
@@ -1,18 +1,19 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { useQuery } from "react-query";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: apiKey
-  }
+    key: apiKey,
+  },
 });
-    
+
 // Fetch genre list
 export const getGenreList = async () => {
   try {
-    const response = await axiosInstance.get('/genres');
+    const response = await axiosInstance.get("/genres");
     return response.data.results;
   } catch (error) {
     throw new Error(`Error fetching genre list: ${error.message}`);
@@ -22,10 +23,10 @@ export const getGenreList = async () => {
 // Fetch game list
 export const getGameList = async () => {
   try {
-    const response = await axiosInstance.get('/games', {
+    const response = await axiosInstance.get("/games", {
       params: {
-        page_size: 40
-      }
+        page_size: 40,
+      },
     });
     return response.data.results;
   } catch (error) {
@@ -34,16 +35,55 @@ export const getGameList = async () => {
 };
 
 // Fetch games by genre ID
-export const getGamesByGenreId = async (genreId) => {
+export const getGamesByGenreId = async (genreId: number) => {
   try {
-    const response = await axiosInstance.get('/games', {
+    const response = await axiosInstance.get("/games", {
       params: {
-        genres: genreId
-      }
+        genres: genreId,
+      },
     });
-    console.log("results:", response.data.results)
     return response.data.results;
   } catch (error) {
-    throw new Error(`Error fetching games by genre ID: ${error.message}`);
+    if (error instanceof AxiosError) {
+      throw new Error(`Error fetching games by genre ID: ${error.message}`);
+    }
   }
+};
+
+export const useGamesByGenreId = ({
+  genreId,
+}: {
+  genreId: number | undefined;
+}) => {
+  const queryParams = genreId ? "?genres=" + genreId : "";
+
+  const query = useQuery({
+    queryKey: ["gamesByGenreId", genreId],
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    refetchInterval: false,
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/games${queryParams}`, {
+        params: {
+          genres: genreId,
+          page_size: 40,
+        },
+      });
+
+      // validate response
+      if (typeof res.data !== "object" && "length" in res.data)
+        throw new Error("Invalid response");
+
+      return res.data.results;
+    },
+    onError: (error) => {
+      console.error("Error fetching games by genre ID:", error);
+    },
+    onSuccess: (data) => {
+      // toast notification
+
+      console.log("Data fetched successfully:", data);
+    },
+  });
+
+  return query;
 };
