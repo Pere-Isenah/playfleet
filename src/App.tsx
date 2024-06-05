5e7f1a7477c053712cc1af50015c924b132ad3a4
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-06-05 21:26:17 +0100

    fix: deprecate old ErrorBoundary component in favor of npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,43 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Header from "./Components/Header.tsx"
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Components/Header.tsx";
 
-import { GameProvider } from "./Context/GameContext"
-import { ThemeContext } from './Context/ThemeContext'
-import ErrorBoundary from "./Components/ErrorBoundary.tsx"
-import { ClerkProvider } from '@clerk/clerk-react'
+import { GameProvider } from "./Context/GameContext";
+import { ThemeContext } from "./Context/ThemeContext";
+import { ErrorBoundary } from "react-error-boundary";
+import { ClerkProvider } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
 import { Outlet, useMatch } from "react-router-dom";
 
 function App() {
   // Import your publishable key
-  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
   if (!PUBLISHABLE_KEY) {
-    throw new Error("Missing Publishable Key")
+    throw new Error("Missing Publishable Key");
   }
 
-  const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState("light");
   const match = useMatch("/");
 
   useEffect(() => {
     // @ts-ignore
-    setTheme(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark')
-  }, [])
+    setTheme(
+      localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark",
+    );
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212]' : null}`}>
+      <div className={`${theme} ${theme == "dark" ? "bg-[#121212]" : null}`}>
         <GameProvider>
-          <ErrorBoundary>
-            <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{ baseTheme: dark }}>
+          <ErrorBoundary
+            fallback={<h1 className="text-2xl">Something went wrong.</h1>}
+          >
+            <ClerkProvider
+              publishableKey={PUBLISHABLE_KEY}
+              appearance={{ baseTheme: dark }}
+            >
               <Header />
               <Outlet />
             </ClerkProvider>
@@ -38,7 +45,8 @@ function App() {
         </GameProvider>
       </div>
     </ThemeContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
+
