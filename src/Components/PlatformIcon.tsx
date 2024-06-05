8bfb09ae2fba628577938940e13bc1b10ac3221f
Pere-Isenah <pereisenah@gmail.com>
2024-06-05 20:42:05 +0100

    resloved more type error

--- src/Components/PlatformIcon.tsx
+++ src/Components/PlatformIcon.tsx
@@ -1,40 +1,32 @@
-import React from 'react';
-import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux } from 'react-icons/fa';
-import { BsNintendoSwitch, BsGlobe } from 'react-icons/bs';
-import { MdPhoneIphone } from 'react-icons/md';
-import { IoLogoAndroid } from 'react-icons/io';
-import { IconType } from 'react-icons/lib';
-
-interface Platform {
-  slug: string;
-}
-
-interface Props {
-  platforms: Platform[];
-}
+import React from 'react'
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux } from "react-icons/fa";
+import { BsNintendoSwitch, BsGlobe} from "react-icons/bs";
+import { MdPhoneIphone } from "react-icons/md";
+import { IoLogoAndroid } from "react-icons/io";
+import { IconType } from "react-icons/lib";
 
 const PlatformIcon = ({ platforms }: Props) => {
-  const IconList: { [key: string]: IconType } = {
-    pc: FaWindows,
-    playstation: FaPlaystation,
-    xbox: FaXbox,
-    mac: FaApple,
-    linux: FaLinux,
-    android: IoLogoAndroid,
-    nintendo: BsNintendoSwitch,
-    ios: MdPhoneIphone,
-    web: BsGlobe,
-  };
-
+  
+  const IconList :{[key: string]: IconType} ={
+    pc: <FaWindows />,
+    playstation: <FaPlaystation />,
+    xbox: <FaXbox />,
+    mac: <FaApple />,
+    linux: <FaLinux />,
+    android: <IoLogoAndroid />,
+    nintendo: <BsNintendoSwitch />,
+    ios: <MdPhoneIphone />,
+    web: <BsGlobe />,
+  }
   return (
     <div>
-      {platforms.map((platform) => (
-        <div className="text-xl dark:text-white" key={platform.slug}>
-          {IconList[platform.slug]}
-        </div>
-      ))}
+      {platforms.map((platform)=>(
+      <div className="text-xl dark:text-white">
+        {IconList[platform.slug]}
+      </div>
+    ))}
     </div>
-  );
-};
+  )
+}
 
-export default PlatformIcon;
+export default PlatformIcon
