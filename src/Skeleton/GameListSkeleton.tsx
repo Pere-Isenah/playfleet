9b4bb6d5dc74ddb24c9f472b692fd1e2ae3f8cde
Pere-Isenah <pereisenah@gmail.com>
2024-06-09 22:11:04 +0100

    update

--- src/Skeleton/GameListSkeleton.tsx
+++ src/Skeleton/GameListSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GameListSkeleton = () => {
   return (
-    <div className='p-5 mt-9 hidden md:block'>
+    <div className='p-5 mt-9'>
       <div className='grid grid-cols-2 gap-6 p-3 h-full lg:grid-cols-4 '>
         {[...Array(20)].map((index) => (
           <div key={index} className='h-full pb-20 p-4 rounded-lg bg-gray-200 dark:bg-gray-700 hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer animate-pulse'>
